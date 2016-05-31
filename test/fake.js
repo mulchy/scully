ee7a05cbbbf21fa646e1af41b18f3e713871d290
mulchy <mulchy81@gmail.com>
2016-05-30 23:39:03 -0500

    Add a fake test to see if CI works.

---
+++ test/fake.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+describe('A fake test to see if CI works', () =>
+  describe('When I have an app, I can write some real tests', () =>
+    it('always passes', () => assert.equal(2 + 2, 4))
+  )
+);
+
